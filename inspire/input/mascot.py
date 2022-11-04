67c15b8e83f9c0fc74f4b4a21b85b4251667c085
John <john.cormican@mpinat.mpg.de>
2022-11-04 10:59:33 +0100

    inSPIRE-1.1 remove unecessary check in source file

--- inspire/input/mascot.py
+++ inspire/input/mascot.py
@@ -174,13 +174,12 @@ def separate_scan_and_source(df_row, scan_title_format, source_list=None, source
         df_row[SCAN_KEY] = int(scan_title.split('=')[-1].strip('~'))
         if source_filename is None:
             source = scan_title.split('File:')[-1]
+            if source.startswith('~'):
+                source = source.split('~')[1]
+            else:
+                source = source.split(', ')[0]
         else:
             source = source_filename
-
-        if source.startswith('~'):
-            source = source.split('~')[1]
-        else:
-            source = source.split(', ')[0]
 
         if source.endswith('.raw') or source.endswith('.mgf'):
             source = source[:-4]
