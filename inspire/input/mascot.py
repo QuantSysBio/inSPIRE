9583552cac8b3a0c4863b7e228b26ea28161a244
John <john.cormican@mpinat.mpg.de>
2022-11-14 10:20:59 +0100

    inSPIRE-1.2-Improvements-for-calibration-and-filtering

--- inspire/input/mascot.py
+++ inspire/input/mascot.py
@@ -351,8 +351,6 @@ def _add_fixed_mod(peptide, ptm_seq, fixed_ptm_dict):
     ptm_seq : str
         The input ptm_seq updated with any relevant fixed modifications.
     """
-    if not isinstance(ptm_seq, str):
-        ptm_seq = '0.' + ''.join(['0']*len(peptide)) + '.0'
     for residues in fixed_ptm_dict:
         if residues == 'N-term':
             if not isinstance(ptm_seq, str):
